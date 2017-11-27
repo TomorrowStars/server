5de3f5d2559e2733625499667ea713d467069b3c
TomorrowStars <chenhongsheng2012@163.com>
2017-11-27 11:15:10 +0800

    20171127_chen_带参数的服务器通讯

--- build/auction_server.js
+++ build/auction_server.js
@@ -2,11 +2,34 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
+var Product = /** @class */ (function () {
+    function Product(id, title, price, rating, desc, categories) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.rating = rating;
+        this.desc = desc;
+        this.categories = categories;
+    }
+    return Product;
+}());
+exports.Product = Product;
+var products = [
+    new Product(1, "第一个商品", 1.99, 3.5, "这是第一个商品，是我在学习慕课网Angular入门实战时创建的", ["电子产品", "硬件设备"]),
+    new Product(2, "第二个商品", 1.99, 2.5, "这是第二个商品，是我在学习慕课网Angular入门实战时创建的", ["电子产品", "硬件设备"]),
+    new Product(3, "第三个商品", 1.99, 4.5, "这是第三个商品，是我在学习慕课网Angular入门实战时创建的", ["图书"]),
+    new Product(4, "第四个商品", 1.99, 1.5, "这是第四个商品，是我在学习慕课网Angular入门实战时创建的", ["电子产品", "软件设备"]),
+    new Product(5, "第五个商品", 1.99, 0.5, "这是第五个商品，是我在学习慕课网Angular入门实战时创建的", ["硬件设备", "硬件设备"]),
+    new Product(6, "第六个商品", 1.99, 3.5, "这是第六个商品，是我在学习慕课网Angular入门实战时创建的", ["图书"])
+];
 app.get('/', function (req, res) {
     res.send("Hello Express!");
 });
 app.get('/products', function (req, res) {
-    res.send("接收到商品查询请求.cccc");
+    res.json(products);
+});
+app.get('/products/:id', function (req, res) {
+    res.json(products.find(function (product) { return product.id == req.params.id; }));
 });
 var server = app.listen(8000, "localhost", function () {
     console.log("服务器已经启动,地址是：http://localhost：8000");
--- server/auction_server.ts
+++ server/auction_server.ts
@@ -2,14 +2,41 @@ import * as express from 'express'
 
 const app = express();
 
-app.get('/', (req,res)=>{
+export class Product {
+    constructor(
+      public id:number,
+      public title:string,
+      public price:number,
+      public rating:number,
+      public desc:string,
+      public categories:Array<string>
+    ) {
+  
+    }
+  }
+
+const products:Product[] = [
+    new Product(1,"第一个商品",1.99,3.5,"这是第一个商品，是我在学习慕课网Angular入门实战时创建的",["电子产品","硬件设备"]),
+    new Product(2,"第二个商品",1.99,2.5,"这是第二个商品，是我在学习慕课网Angular入门实战时创建的",["电子产品","硬件设备"]),
+    new Product(3,"第三个商品",1.99,4.5,"这是第三个商品，是我在学习慕课网Angular入门实战时创建的",["图书"]),
+    new Product(4,"第四个商品",1.99,1.5,"这是第四个商品，是我在学习慕课网Angular入门实战时创建的",["电子产品","软件设备"]),
+    new Product(5,"第五个商品",1.99,0.5,"这是第五个商品，是我在学习慕课网Angular入门实战时创建的",["硬件设备","硬件设备"]),
+    new Product(6,"第六个商品",1.99,3.5,"这是第六个商品，是我在学习慕课网Angular入门实战时创建的",["图书"])
+  ];
+
+app.get('/', (req, res) => {
     res.send("Hello Express!");
 });
 
-app.get('/products', (req, res) =>{
-    res.send("接收到商品查询请求.cccc");
+app.get('/products', (req, res) => {
+    res.json(products);
 });
 
-const server = app.listen(8000, "localhost",() =>{
+app.get('/products/:id', (req, res) => {
+    res.json(products.find((product) => product.id == req.params.id));
+});
+
+const server = app.listen(8000, "localhost", () => {
     console.log("服务器已经启动,地址是：http://localhost：8000");
 });
+
